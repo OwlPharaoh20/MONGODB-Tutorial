adbd4c856f1bec1d429eb187ef6f34025b1144c1
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-13 00:18:34 -0700

    Database auth done, cruising smooth now

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://oxdiablo:Pollos_Hermanos505@cluster0.dtq5smd.mongodb.net/')
+mongoose.connect('mongodb+srv://oxdiablo20:Pollos_Hermanos505@cluster0.dtq5smd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .catch((error) => {
   console.error('Error connecting to MongoDB:', error);
 });
@@ -14,3 +14,33 @@ db.on('error', (error) => {
 db.once("open", () => {
   console.log("Database Connected")
 });
+
+//CRUD 
+
+//CREATE READ UPDATE DELETE 
+/*
+Steps to perform CRUD OPERATIONS
+1. Create a schema 
+2. Create a model 
+3. Perform the operation 
+*/
+
+//Create User schema
+
+const userSchema = new mongoose.Schema({
+    firstName: String ,
+    lastName: String,
+    age: Number,
+    isMarried: Boolean,
+})
+
+//Step 2: Making the model 
+const User = mongoose.model('user', userSchema);
+
+//Create a new user 
+const newUser = new User({
+    firstName: "Clem",
+    lastName: "Brighton",
+    age: 53,
+    isMarried : false, 
+})
